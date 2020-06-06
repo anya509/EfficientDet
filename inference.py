ff01eea4939991a3d9088b22e19584b1f90ad42a
anya509 <52094963+anya509@users.noreply.github.com>
2020-06-06 21:44:46 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -13,11 +13,11 @@ from utils.draw_boxes import draw_boxes
 def main():
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-    phi = 1
-    weighted_bifpn = True
+    phi = 1 #changes might be required here
+    weighted_bifpn = True 
     model_path = 'efficientdet-d1.h5'
-    image_sizes = (512, 640, 768, 896, 1024, 1280, 1408)
-    image_size = image_sizes[phi]
+    image_sizes = (512, 640, 768, 896, 1024, 1280, 1408) #changes might be required here
+    image_size = image_sizes[phi] #changes might be required here
     # coco classes
     classes = {value['id'] - 1: value['name'] for value in json.load(open('coco_90.json', 'r')).values()}
     num_classes = 90
@@ -30,13 +30,13 @@ def main():
     model.load_weights(model_path, by_name=True)
 
     for image_path in glob.glob('datasets/VOC2007/JPEGImages/*.jpg'):
-        image = cv2.imread(image_path)
+        image = cv2.imread(image_path) # see once here
         src_image = image.copy()
         # BGR -> RGB
         image = image[:, :, ::-1]
         h, w = image.shape[:2]
 
-        image, scale = preprocess_image(image, image_size=image_size)
+        image, scale = preprocess_image(image, image_size=image_size) #changes might be required here
         # run network
         start = time.time()
         boxes, scores, labels = model.predict_on_batch([np.expand_dims(image, axis=0)])
